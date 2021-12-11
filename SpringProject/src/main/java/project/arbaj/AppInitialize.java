f716bc34b1692f8dd39abeefb636d9c1270dfc76
ArbajAli <arbajmessiali@outlook.com>
2021-12-11 20:55:56 +0530

    Initial Push

---
+++ SpringProject/src/main/java/project/arbaj/AppInitialize.java
@@ -0,0 +1,11 @@
+package project.arbaj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppInitialize {
+	public static void main(String[] args) {
+		SpringApplication.run(AppInitialize.class, args);
+	}
+}
---
+++ SpringProject/src/webapp/index.jsp
@@ -0,0 +1,5 @@
+<html>
+<body>
+<h2>Welcome to the Register</h2>
+</body>
+</html>
