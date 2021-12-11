0d48030b10778d3bd0447e167f95eddc18e2dfca
ArbajAli <arbajmessiali@outlook.com>
2021-12-11 21:25:05 +0530

    trying few changes

--- SpringProject/src/main/java/project/arbaj/control/AppController.java
+++ SpringProject/src/main/java/project/arbaj/control/AppController.java
@@ -1,12 +1,11 @@
 package project.arbaj.control;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-@RequestMapping(path ="/project")
+@RequestMapping(path ="/")
 public class AppController {
 	
 	@RequestMapping("/index")
